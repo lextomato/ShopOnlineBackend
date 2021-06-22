4a20472065d9c26fe6bed723e5c3324ad981ba5f
Jhojany Uzcategui <jhojaforce@gmail.com>
2021-06-22 16:47:34 -0400

    no coments

--- shop/settings.py
+++ shop/settings.py
@@ -35,7 +35,8 @@ ALLOWED_HOSTS = [
 
 # Application definition
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     "https://shop-online-forntend.herokuapp.com",
 ]
